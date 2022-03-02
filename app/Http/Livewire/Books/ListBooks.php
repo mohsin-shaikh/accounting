de39fcdb9eac932a160d5f0460b73f18246272a6
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2022-03-02 11:59:59 +0530

    Added Scout Search on Filament Table

--- app/Http/Livewire/Books/ListBooks.php
+++ app/Http/Livewire/Books/ListBooks.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Livewire\Books;
 
+use Closure;
+use Filament\Forms;
 use App\Models\Book;
 use Filament\Tables;
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
 class ListBooks extends Component implements Tables\Contracts\HasTable
 {
     use Tables\Concerns\InteractsWithTable;
+
+    protected $queryString = [
+        'tableFilters',
+        'tableSortColumn',
+        'tableSortDirection',
+        'tableSearchQuery' => ['except' => ''],
+    ];
 
     protected function getTableQuery(): Builder
     {
@@ -23,6 +33,25 @@ class ListBooks extends Component implements Tables\Contracts\HasTable
             Tables\Columns\TextColumn::make('name'),
             Tables\Columns\TextColumn::make('user.name'),
         ];
+    }
+
+    protected function getTableRecordUrlUsing(): Closure
+    {
+        return fn (Model $record): string => route('books.edit', ['book' => $record]);
+    }
+
+    public function isTableSearchable(): bool
+    {
+        return true;
+    }
+
+    protected function applySearchToTableQuery(Builder $query): Builder
+    {
+        if (filled($searchQuery = $this->getTableSearchQuery())) {
+            $query->whereIn('id', Book::search($searchQuery)->keys());
+        }
+
+        return $query;
     }
 
     public function render(): View
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,14 +3,15 @@
 namespace App\Models;
 
 use App\Scopes\BookScope;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $fillable = ['name', 'user_id'];
 
